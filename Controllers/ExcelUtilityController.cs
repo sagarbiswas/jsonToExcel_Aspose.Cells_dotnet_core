57ac14d9f3332eaaca55a865732cf357ee351abe
Sagar <sagarbiswasin@gmail.com>
2024-03-29 14:11:33 +0530

    file name added.

--- Controllers/ExcelUtilityController.cs
+++ Controllers/ExcelUtilityController.cs
@@ -9,37 +9,38 @@ namespace JsonToExcel.Controllers
     public class ExcelUtilityController : ControllerBase
     {
 
-    [HttpPost("DownloadExcel")]
-    public Task<IActionResult> DownloadExcel(ExcelUtilModel excelUtil)
-    {
+        [HttpPost("DownloadExcel")]
+        public Task<IActionResult> DownloadExcel(ExcelUtilModel excelUtil)
+        {
 
-        Workbook workbook = new Workbook();
-        Worksheet worksheet = workbook.Worksheets[0];
+            Workbook workbook = new Workbook();
+            Worksheet worksheet = workbook.Worksheets[0];
 
-        CellsFactory factory = new CellsFactory();
-        Style style = factory.CreateStyle();
-        style.HorizontalAlignment = TextAlignmentType.Center;
-        style.Font.IsBold = true;
+            CellsFactory factory = new CellsFactory();
+            Style style = factory.CreateStyle();
+            style.HorizontalAlignment = TextAlignmentType.Center;
+            style.Font.IsBold = true;
 
-        // Set JsonLayoutOptions
-        JsonLayoutOptions options = new JsonLayoutOptions();
-        options.TitleStyle = style;
-        options.ArrayAsTable = true;
+            // Set JsonLayoutOptions
+            JsonLayoutOptions options = new JsonLayoutOptions();
+            options.TitleStyle = style;
+            options.ArrayAsTable = true;
 
-            // Import JSON Data
+            // You can Import JSON Data form json file too.
             JsonUtility.ImportData(excelUtil.JsonInput, worksheet.Cells, 0, 0, options);
 
             MemoryStream ms = new MemoryStream();
-        workbook.Save(ms, SaveFormat.Excel97To2003);
-        ms.Seek(0, SeekOrigin.Begin);
+            workbook.Save(ms, SaveFormat.Excel97To2003);
+            ms.Seek(0, SeekOrigin.Begin);
 
-        byte[] buffer = new byte[(int)ms.Length];
-        buffer = ms.ToArray();
-        if (buffer == null)
-            return Task.FromResult<IActionResult>(NotFound()); // returns a NotFoundResult with Status404NotFound response.
 
-        return Task.FromResult<IActionResult>(File(buffer, "application/octet-stream", "xyz.xls")); // returns a FileStreamResult
-    }
+            byte[] buffer = new byte[(int)ms.Length];
+            buffer = ms.ToArray();
+            if (buffer == null)
+                return Task.FromResult<IActionResult>(NotFound()); // returns a NotFoundResult with Status404NotFound response.
+
+            return Task.FromResult<IActionResult>(File(buffer, "application/octet-stream", $"{excelUtil.FileName}.xls")); // returns a FileStreamResult
+        }
 
     }
 }
